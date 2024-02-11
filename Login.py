6e297fe7605ce5c1a67c08c3388940ce60c325b6
Anxhelo Kambo <88976322+ChimerG@users.noreply.github.com>
2024-02-10 23:21:49 -0500

    Update Login.py
    Anxhelo updated user_login and create_account

--- Login.py
+++ Login.py
@@ -3,65 +3,83 @@
 # Description: Basic features for InCollege
 import os
 
-
 def create_account():
-  # Output messages to user and take username input
-  print("\nPlease enter the following information to create an account.\n")
-  username = input("Username: ")
-  # Open file for read, check for existing username and number of users
-  numUsers = 1
-  with open("accounts.txt", "r+") as file1:
-    for line in file1:
-      numUsers += 1
-      if username in line:
-        print("\nUsername already exists. Please try again.")
-        create_account()
-        return
-      elif numUsers == 6:
-        print("\nAll permitted accounts have been created, please come back later")
-        exit()
-  print("\nUsername is Available!\n")
+    # Output messages to user and take username input
+    print("\nPlease enter the following information to create an account.\n")
+    username = input("Username: ")
     
-  # Output messages to user and take password input
-  print("(password must be a minimum of 8 characters, maximum of 12 characters, contain"
-  " at least one uppercase letter, one digit, and one special character)") 
-  password = input("Password: ")
+    # Open file for read, check for existing username and number of users
+    numUsers = 1
+    with open("accounts.txt", "r+") as file1:
+        for line in file1:
+            numUsers += 1
+            if username in line:
+                print("\nUsername already exists. Please try again.")
+                create_account()
+                return
+            elif numUsers == 6:
+                print("\nAll permitted accounts have been created, please come back later")
+                exit()
+    print("\nUsername is Available!\n")
 
-  # Checking if password meets requirements
-  if len(password) < 8 or len(password) > 12:
-    print("\nPassword must be between 8 and 12 characters long.")
-    print("Please try again.")
-    create_account()
-  elif not any(char.isdigit() for char in password):
-    print("\nPassword must contain at least one digit.")
-    print("Please try again.")
-    create_account()
-  elif not any(char.isupper() for char in password):
-    print("\nPassword must contain at least one uppercase letter.")
-    print("Please try again.")
-    create_account()
-  elif not any(not char.isalnum() for char in password):
-    print("\nPassword must contain at least one special character.")
-    print("Please try again.")
-    create_account()
-  else:
-    # Password meets requirements, create account if password is confirmed
-    confirm_password = input("Confirm Password: ")
-    if password == confirm_password:
-      # Open file for append, write username and password to file
-      with open("accounts.txt", "a") as file2:
-        file2.write('\n' + username + " " + password)
-      print("\nAccount created successfully!")
-      member_options()
+    # Output messages to user and take password input
+    print("(password must be a minimum of 8 characters, maximum of 12 characters, contain"
+          " at least one uppercase letter, one digit, and one special character)")
+    password = input("Password: ")
+
+    # Asking for first name and last name
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+
+    # Checking if password meets requirements
+    if len(password) < 8 or len(password) > 12:
+        print("\nPassword must be between 8 and 12 characters long.")
+        print("Please try again.")
+        create_account()
+    elif not any(char.isdigit() for char in password):
+        print("\nPassword must contain at least one digit.")
+        print("Please try again.")
+        create_account()
+    elif not any(char.isupper() for char in password):
+        print("\nPassword must contain at least one uppercase letter.")
+        print("Please try again.")
+        create_account()
+    elif not any(not char.isalnum() for char in password):
+        print("\nPassword must contain at least one special character.")
+        print("Please try again.")
+        create_account()
     else:
-      print("\nPasswords do not match. Please try again.")
-      create_account()
-  
-  return
+        # Password meets requirements, create account if password is confirmed
+        confirm_password = input("Confirm Password: ")
+        if password == confirm_password:
+            # Open file for append, write username, password, first name, and last name to file
+            with open("accounts.txt", "a") as file2:
+                file2.write('\n' + username + " " + password + " " + first_name + " " + last_name)
+            print("\nAccount created successfully!")
+            member_options()
+        else:
+            print("\nPasswords do not match. Please try again.")
+            create_account()
+    return   
 
 
 
 def user_login():
+
+  # Display college student success story
+    print("\nWelcome to InCollege!")
+    print("Here's a success story from one of our college students:")
+    print("Jane Doe, a student at University of South Florida, used InCollege to find her dream job at ABC Company. ")
+    print("She credits InCollege for helping her build her professional network and discover job opportunities.")
+    
+    # Prompt user to watch a video
+    choice = input("\nWould you like to watch a video about InCollege? (yes/no): ")
+    if choice.lower() == "yes":
+        print("\nVideo is now playing.")
+        # Code to play the video goes here
+    else:
+        print("You can explore InCollege features further by signing in or creating an account.")
+
   # Output messages to user and take username input
   print("\nPlease enter your username and password to login.\n")
   username = input("Username: ")
@@ -76,6 +94,8 @@ def user_login():
         return
   print("Incorrect username / password, please try again.")
   user_login()
+
+
 
 
 
